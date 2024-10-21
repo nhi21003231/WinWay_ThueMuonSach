d098133b75db2e0cea2e323f49a6bfa75296cca1
duycuong712 <nguyenduycuong712@gmail.com>
2024-10-21 22:29:54 +0700

    sua css html dang bao

--- ThueMuonSach/resources/views/CuaHang/pages/NhanVien/DangBaiBao/index.blade.php
+++ ThueMuonSach/resources/views/CuaHang/pages/NhanVien/DangBaiBao/index.blade.php
@@ -5,16 +5,16 @@
 <!-- resources/views/create_article.blade.php -->
  
 <body>
-    <form method="POST" action="">
+    <form class="dangbao-form" method="POST" action="">
         @csrf
-        <label for="title">Tiêu Đề:</label>
-        <input type="text" id="title" name="title" required>
+        <label class="dangbao-label" for="title">Tiêu Đề:</label>
+        <input class="dangbao-input" type="text" id="title" name="title" required>
 
-        <label for="content">Nội Dung:</label>
-        <textarea id="content" name="content" required></textarea>
+        <label class="dangbao-label" for="content">Nội Dung:</label>
+        <textarea class="dangbao-textarea" id="content" name="content" required></textarea>
 
-        <button type="submit">Đăng</button>
-        <button type="button" onclick="clearForm()">Xóa Toàn Bộ Nội Dung</button>
+        <button class="dangbao-button" type="submit">Đăng</button>
+        <button class="dangbao-button dangbao-button-danger" type="button" onclick="clearForm()">Xóa Toàn Bộ Nội Dung</button>
     </form>
 
     <script>
@@ -24,5 +24,4 @@
         }
     </script>
 </body>
-</html>
 @endsection
