e935a87536470b2f835b40ab652fbbd6bd1bf779
nhi21003231 <21003231.nhi@student.iuh.edu.vn>
2024-10-21 22:06:01 +0700

    trang chủ của khách hàng

--- ThueMuonSach/resources/views/KhachHang/layouts/footer.blade.php
+++ ThueMuonSach/resources/views/KhachHang/layouts/footer.blade.php
@@ -1,1 +1,47 @@
-<h1>Footer</h1>
+<!-- <h1>Footer</h1> -->
+<footer class="footer spad">
+        <div class="container">
+            <div class="row" style="margin: 20px 0 10px">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__about__logo" style="line-height: 150px">
+                            <a href="./index.html"><img src="img/logo1.png" alt="" width="50%" style="margin-left: 170px;"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
+                    <div class="footer__widget">
+                        <h4>Truy cập nhanh</h4>
+                        <ul style="list-style-type: none; color: #333; margin: 10px 0 0 0">
+                            <li><a style="color: #333" href="{{ route('route-khachhang-trangchu') }}">Home</a></li>
+                            <li><a style="color: #333" href="{{ route('route-khachhang-chitietanpham') }}">Ấn Phẩm</a></li>
+                            <li><a style="color: #333" href="{{ route('route-khachhang-lienhe') }}">Liên hệ</a></li>
+                            <li><a style="color: #333" href="{{ route('route-khachhang-chinhsach') }}">Chính sách</a></li>
+                            <li><a style="color: #333" href="{{ route('route-khachhang-giohang') }}">Quản lý đơn hàng</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="footer__widget">
+                        <h4>Liên hệ</h4>
+                        <div class="footer__widget__social" style="margin: 10px 0 0 0">
+                            <ul style="list-style-type: none; color: #333">
+                                <li>Address: 12 Nguyễn Văn Bảo</li>
+                                <li>Phone: 09.8888.9898</li>
+                                <li>Email: VoVanNhi_WindWayShop</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12" style="text-align: center !important; border-top: 1px solid #ccc; margin: 15px 0 0 0">
+                    <div class="footer__copyright">
+                        <div class="footer__copyright__text" style="width: 100%; margin: 10px 0px 0 4%">
+                            <p> Copyright &copy; NumberTwo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
